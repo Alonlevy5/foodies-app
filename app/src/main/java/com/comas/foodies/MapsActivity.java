aa6e3f1925c683f93e4121959cae4d4f045b9b9d
DanSasi <dan.sasi60@gmail.com>
2022-03-09 10:46:48 +0200

    need to Fix Current Map

---
+++ app/src/main/java/com/comas/foodies/MapsActivity.java
@@ -0,0 +1,170 @@
+package com.comas.foodies;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+
+public class MapsActivity extends AppCompatActivity {
+
+    private GoogleMap mMap;
+    private SupportMapFragment mapFragment;
+    private FusedLocationProviderClient client;
+    private final int REQUEST_CODE = 111;
+    private ConnectivityManager manager;
+    private NetworkInfo networkInfo;
+    private Geocoder geocoder;
+    private double selectedLat ,selectedLng ;
+    private List<Address> addresses;
+    private String selectedAddress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps_current_place);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.maps);
+        client = LocationServices.getFusedLocationProviderClient(this);
+
+        if (ActivityCompat.checkSelfPermission(MapsActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            getCurrentLocation();
+        } else {
+            ActivityCompat.requestPermissions(MapsActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+        }
+
+    }
+
+    private void getCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Task<Location> task = client.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(final Location location) {
+                if(location != null) {
+                    mapFragment.getMapAsync(new OnMapReadyCallback() {
+                        @Override
+                        public void onMapReady(@NonNull GoogleMap googleMap) {
+                            mMap = googleMap;
+                            LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
+                            googleMap.addMarker(new MarkerOptions().position(latLng).title("You are here"));
+                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 14));
+
+                            mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                                @Override
+                                public void onMapClick(@NonNull LatLng latLng) {
+                                    CheckConnection();
+
+                                    if(networkInfo.isConnected() && networkInfo.isAvailable()){
+                                        selectedLat = latLng.latitude;
+                                        selectedLng= latLng.longitude;
+
+                                        GetAddress(selectedLat,selectedLng);
+
+
+                                    }else {
+                                        Toast.makeText(MapsActivity.this,
+                                                "Please Check Connection", Toast.LENGTH_SHORT).show();
+                                    }
+
+                                }
+                            });
+                        }
+                    });
+
+                }
+
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if(requestCode == REQUEST_CODE)
+        {
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED ){
+                getCurrentLocation();
+            }
+        }else
+        {
+            Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void CheckConnection(){
+        manager = (ConnectivityManager) getApplicationContext().getSystemService(CONNECTIVITY_SERVICE);
+        networkInfo = manager.getActiveNetworkInfo();
+    }
+
+    private void GetAddress(double mLat, double mLng){
+        geocoder = new Geocoder(MapsActivity.this, Locale.getDefault());
+        if(mLat != 0){
+            try {
+                addresses = geocoder.getFromLocation(mLat, mLng, 1);
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+            if(addresses != null){
+                String mAddress = addresses.get(0).getAddressLine(0);
+                String city = addresses.get(0).getLocality();
+                String state = addresses.get(0).getAdminArea();
+                String postalCode = addresses.get(0).getPostalCode();
+                String knownName = addresses.get(0).getFeatureName();
+                String dis = addresses.get(0).getSubAdminArea();
+
+                selectedAddress = mAddress;
+
+                if(mAddress != null){
+                    LatLng latLng = new LatLng(mLat,mLng);
+                    mMap.addMarker(new MarkerOptions().position(latLng).title(selectedAddress));
+                }else {
+                    Toast.makeText(this,"Something went wrong", Toast.LENGTH_SHORT).show();
+                }
+            }else {
+                Toast.makeText(this, "Something went wrong", Toast.LENGTH_SHORT).show();
+            }
+        }else{
+            Toast.makeText(this," LatLng null", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
